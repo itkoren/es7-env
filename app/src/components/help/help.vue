3b7f95096ac2c403d4c3597b7d8b0081cf62a6b1
eliranm <eliranm@liveperson.com>
2017-06-26 14:15:59 +0300

    add help overlay

---
+++ app/src/components/help/help.vue
@@ -0,0 +1,112 @@
+<template>
+  <div class="help">
+    <button type="button" class="trigger btn btn-lg btn-outline-primary ml-5" aria-label="Show Help" @click="show">
+      <span aria-hidden="true">&quest;</span>
+    </button>
+    <div class="overlay" v-show="!hidden">
+      <button type="button" class="close" aria-label="Hide Help" @click="hide">
+        <span aria-hidden="true">&times;</span>
+      </button>
+      <div class="content" @click="hide">
+        <h1 class="text-center">ES7 help</h1>
+        <h2>Key map</h2>
+        <p class="pb-3">Some key-bindings worth knowing. For the full list, <a href="https://codemirror.net/doc/manual.html#commands" target="_blank" rel="noopener noreferrer">see here</a>.</p>
+        <div v-for="details in keyMap">
+          <h3>{{details.name}}<span class="float-right">
+                <span v-if="!!details.keys.all"><kbd v-for="allKey in details.keys.all">{{allKey}}</kbd></span>
+                <span v-if="!!details.keys.pc">pc: <kbd v-for="pcKey in details.keys.pc">{{pcKey}}</kbd></span>
+                <span v-if="!!details.keys.mac">mac: <kbd v-for="macKey in details.keys.mac">{{macKey}}</kbd></span>
+              </span></h3>
+          <p>{{details.description}}</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+  import keyMap from './key-map';
+
+  export default {
+    data () {
+      return {
+        keyMap,
+        hidden: true
+      };
+    },
+    methods: {
+      show () {
+        this.hidden = false;
+      },
+      hide () {
+        this.hidden = true;
+      },
+    },
+  };
+
+</script>
+
+<style scoped>
+
+  h1, h2, h3 {
+    margin: 1em 0 .5em;
+    color: #fff;
+  }
+  h3 {
+    font-size: 1.4rem;
+  }
+  a {
+    color: #f2f2f2;
+    text-decoration: none;
+    border-bottom: 1px solid;
+  }
+  kbd + kbd {
+    margin-left: .2em;
+  }
+
+  .overlay,
+  .content {
+    position: fixed;
+  }
+
+  .overlay {
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    z-index: 99999;
+    opacity: .8;
+    background-color: #f2f2f2;
+  }
+
+  .content {
+    font-size: 1.2rem;
+    top: 3rem;
+    bottom: 3rem;
+    left: 3rem;
+    right: 3rem;
+    overflow-y: auto;
+    padding: 2em;
+    color: #e0e0e0;
+    background-color: #555;
+  }
+
+  .trigger {
+    border-radius: 50% !important;
+    width: 2em;
+    height: 2em;
+    padding: 0;
+  }
+
+  .close {
+    width: 1.5em;
+    height: 1.5em;
+    margin: 0.5em 0.5em 0 0;
+  }
+
+  .close:focus {
+    outline: 0 none;
+  }
+
+</style>
---
+++ app/src/components/help/key-map.js
@@ -0,0 +1,97 @@
+export default [
+
+  {
+    name: 'Run',
+    keys: {
+      all: ['Ctrl-Enter']
+    },
+    description: 'Executes the current content of the editor.'
+  },
+
+  {
+    name: 'Auto Complete',
+    keys: {
+      all: ['Ctrl-Space']
+    },
+    description: 'Completes the term before the cursor, or opens a list of terms to choose from.'
+  },
+
+  {
+    name: 'Indent Auto',
+    keys: {
+      all: ['Shift-Tab']
+    },
+    description: 'Auto-indent the current line or selection.'
+  },
+
+  {
+    name: 'Transpose Chars',
+    keys: {
+      mac: ['Ctrl-T']
+    },
+    description: 'Swap the characters before and after the cursor.'
+  },
+
+  {
+    name: 'Delete Line',
+    keys: {
+      pc: ['Ctrl-D'],
+      mac: ['Cmd-D']
+    },
+    description: 'Deletes the whole line under the cursor, including newline at the end.'
+  },
+
+  {
+    name: 'Undo',
+    keys: {
+      pc: ['Ctrl-Z'],
+      mac: ['Cmd-Z']
+    },
+    description: 'Undo the last change.'
+  },
+
+  {
+    name: 'Redo',
+    keys: {
+      pc: ['Ctrl-Y'],
+      mac: ['Shift-Cmd-Z', 'Cmd-Y']
+    },
+    description: 'Redo the last undone change.'
+  },
+
+  {
+    name: 'Select All',
+    keys: {
+      pc: ['Ctrl-A'],
+      mac: ['Cmd-A']
+    },
+    description: 'Select the whole content of the editor.'
+  },
+
+  {
+    name: 'Single Selection',
+    keys: {
+      all: ['Esc']
+    },
+    description: 'When multiple selections are present, this deselects all but the primary selection (you can make multiple selections, by the way).'
+  },
+
+  {
+    name: 'Undo Selection',
+    keys: {
+      pc: ['Ctrl-U'],
+      mac: ['Cmd-U']
+    },
+    description: 'Undo the last change to the selection, or if there are no selection-only changes at the top of the history, undo the last change.'
+  },
+
+  {
+    name: 'Redo Selection',
+    keys: {
+      pc: ['Alt-U'],
+      mac: ['Shift-Cmd-U']
+    },
+    description: 'Redo the last change to the selection, or the last text change if no selection changes remain.'
+  },
+
+]
