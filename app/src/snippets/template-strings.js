23b1de2cefea47342aa5a438c8b84582cb3046d9
eliranm <eliranm@liveperson.com>
2017-07-09 16:26:42 +0300

    minor changes

--- app/src/snippets/template-strings.js
+++ app/src/snippets/template-strings.js
@@ -1,20 +1,26 @@
-// 'Hi\\n5!', the character after 'Hi' is not a newline character, '\' and 'n' are two characters.
-console.log(String.raw`Hi\n${2+3}!`);
+// the character after 'Hi' is not a newline character, '\' and 'n' are two characters.
+console.log(String.raw`Hi\n${2 + 3}!`);
+console.dir();
 
-// 'Hi\\u000A!', same here, this time we will get the \, u, 0, 0, 0, A, 6 characters.
-// All kinds of escape characters will be ineffective and backslashes will be present in the output string.
-// You can confirm this by checking the .length property of the string.
+// same here, this time we will get the \, u, 0, 0, 0, A; 6 characters.
+// all kinds of escape characters will be ineffective and backslashes will be present in the output string.
+// you can confirm this by checking the .length property of the string.
 console.log(String.raw`Hi\u000A!`);
+console.log(String.raw`Hi\u000A!`.length);
+console.dir();
 
-// 'Hi\\nBob!', substitutions are processed.
+// substitutions are processed.
 const name = 'Bob';
 console.log(String.raw`Hi\n${name}!`);
+console.dir();
 
-// Normally you would not call String.raw() as a function, but to simulate `t${0}e${1}s${2}t` you can do:
-console.log(String.raw({ raw: 'test' }, 0, 1, 2));
+// normally you would not call String.raw() as a function, but to simulate `t${0}e${1}s${2}t` you can do:
+console.log(String.raw({raw: 'test'}, 0, 1, 2));
+console.dir();
 
-// Note that 'test', a string, is an array-like object. The following is equivalent to:
+// note that 'test', a string, is an array-like object. The following is equivalent to:
 // `foo${2 + 3}bar${'Java' + 'Script'}baz`
 console.log(String.raw({
   raw: ['foo', 'bar', 'baz']
 }, 2 + 3, 'Java' + 'Script'));
+console.dir();
