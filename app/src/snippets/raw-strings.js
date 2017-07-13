9c0a0d32b1867c5fd41dc7bb59c34884e6548e4f
eliranm <eliranm@liveperson.com>
2017-07-13 18:04:06 +0300

    update raw strings snippet

--- app/src/snippets/raw-strings.js
+++ app/src/snippets/raw-strings.js
@@ -1,5 +1,5 @@
 // '\n' will not become a newline character, but two separate characters: '\' and 'n'
-console.log(String.raw`hey.\n been trying to meet you`);
+console.log(String.raw`hey,\n been trying to meet you`);
 console.dir();
 
 // similarly, '\u000A' is not an escape sequence anymore: check the string length for proof
@@ -12,14 +12,14 @@ const consequence = 'die';
 console.log(String.raw`if you ${action}, i will surely ${consequence}`);
 console.dir();
 
-// normally you would not call String.raw() as a function, but to simulate `|${0}|${1}|${2}|` you can do:
+// normally you would not call String.raw() as a function, but to simulate `6${'*'}7${'='}42` you can do:
 console.log(String.raw({
-  raw: '||||'
-}, 0, 1, 2));
+  raw: '6742'
+}, '*', '='));
 console.dir();
 
 // the above works as a string is an array-like object. we could pass in an actual array instead:
 console.log(String.raw({
-  raw: [1, 2, 3]
-}, '+', '='));
+  raw: ['must be a ', ' between us']
+}, 'devil'));
 console.dir();
