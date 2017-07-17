67f3ad03cdc513f37c19785acf9f07c135a92cd6
eliranm <eliranm@liveperson.com>
2017-07-18 00:42:33 +0300

    add use case for empty slots fix

--- app/src/snippets/array-mutation.js
+++ app/src/snippets/array-mutation.js
@@ -1,17 +1,46 @@
+/* 'array-mutation' */
+
 let altered;
+
+const printer = (v, i) => console.log(
+  `» index: ${i}, value: ${JSON.stringify(v)}`
+);
+
+console.dir();
+console.info('--- Array.prototype.fill() ---');
+console.dir();
+// fills all the specified elements with a static value
+
+const flags = [1,0,1,0,0];
+altered = flags.fill(0);
+
+console.log(altered);
+console.log(flags);
+console.dir();
+
+// use-case: fix holes in array construction
+
+const emptySlots = new Array(3);
+const emptyStrings = (new Array(3)).fill('');
+
+console.log('empty slots:')
+emptySlots.forEach(printer);
+console.log('empty strings:')
+emptyStrings.forEach(printer);
+
 
 console.dir();
 console.info('--- Array.prototype.copyWithin() ---');
 console.dir();
 // shallow-copies part of an array to another location in the same array
 
-const arrows = ['❮', '❯', '❯', '❯'];
-const len = arrows.length;
-altered = arrows.copyWithin(1);
+const shapes = ['▲', '●', '●', '●'];
+const len = shapes.length;
+altered = shapes.copyWithin(1);
 
 console.log(altered);
 // length is maintained
-console.log(arrows.length === len);
+console.log(altered.length === len);
 console.dir();
 
 const line = ['everything you own in a box', 'to the left'];
