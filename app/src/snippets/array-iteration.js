6acf4d56f84f1f047aeab6edac8527c3a36e516d
eliranm <eliranm@liveperson.com>
2017-07-18 01:08:26 +0300

    add array iteration snippet

---
+++ app/src/snippets/array-iteration.js
@@ -0,0 +1,8 @@
+
+// the range-generator magic trick:
+const range = [...Array(7).keys()];
+
+// what's going on:
+// 1. a new array is created, with N empty slots
+// 2. keys() returns an iterator with N index numbers as its values
+// 3. the spread shallow-copies them onto a new array
--- build/files.js
+++ build/files.js
@@ -103,6 +103,7 @@ const files = [
       'array-builders',
       'array-mutation',
       'array-searching',
+      'array-iteration',
     ]
   },
   {
