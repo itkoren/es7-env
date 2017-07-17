48736c3d1c64d735029e8e91316c4bc2caf845ad
eliranm <eliranm@liveperson.com>
2017-07-18 02:12:46 +0300

    update array iteration snippet

--- app/src/snippets/array-iteration.js
+++ app/src/snippets/array-iteration.js
@@ -1,10 +1,24 @@
+console.dir(`
+--- Array.prototype.keys() ---
+`);
 
 // the range-generator magic trick:
-const range = [...Array(7).keys()];
+
+let range;
+
+console.info('zero-based:');
+
+range = [...Array(7).keys()];
+console.log(range);
+console.dir();
+
+console.info('start offset of 10:');
+
+[,,,,,,,,,, ...range] = [...Array(14).keys()];
+console.log(range);
+console.dir();
 
 // what's going on:
 // 1. a new array is created, with N empty slots
 // 2. keys() returns an iterator with N index numbers as its values
 // 3. the spread shallow-copies them onto a new array
-
-console.log(range);
