c7b0542583db6798f9754c3ca0f05a14bfff5af3
Itai Koren <itaik@liveperson.com>
2017-07-24 15:01:12 +0300

    fetch api snippets

--- app/src/snippets/async.fetch.js
+++ app/src/snippets/async.fetch.js
@@ -6,9 +6,53 @@ console.log('');
 
 const url = 'https://jsonplaceholder.typicode.com/posts';
 fetch(url)
-  .then(data => {
-    for (const key in data) {
-      console.log(`${key}:${JSON.stringify(data[key])}`);
+  .then(reply => {
+    console.log(reply.toString());
+    console.log('');
+    console.log('');
+    for (const key in reply) {
+      console.log(`${key}:${JSON.stringify(reply[key])}`);
     }
+    console.log('');
+    console.log('');
+    return reply;
+  })
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .then(() => {
+    console.log('');
+    console.log('##########################');
+    console.log('##########################');
+    console.log('##########################');
+    console.log('');
+
+    whyLikeThis();
   });
 
+
+
+
+console.log('');
+console.log('##########################');
+console.log('##########################');
+console.log('##########################');
+console.log('');
+
+
+const whyLikeThis = () => {
+  //const url = 'https://jsonplaceholder.typicode123.com/posts';
+
+  fetch(`${url}1234`)
+    .then(reply => {
+      console.log(reply.toString());
+      console.log('');
+      console.log('');
+      for (const key in reply) {
+        console.log(`${key}:${JSON.stringify(reply[key])}`);
+      }
+      console.log('');
+      console.log('');
+      return reply;
+    })
+    .catch(err => console.error(err));
+};
