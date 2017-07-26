e281b1316d0af7d5e1488f6b060ad4e534fa12dc
eliranm <eliranm@liveperson.com>
2017-07-26 11:38:20 +0300

    update beats-and-bytes snippet

--- app/src/snippets/beats-and-bytes.js
+++ app/src/snippets/beats-and-bytes.js
@@ -1,84 +1,10 @@
 
 const ø = Object.create(null);
-const speaker = String.fromCodePoint('0x1f509');
-const folder = String.fromCodePoint('0x1f4c1');
-const keyboard = String.fromCodePoint('0x2328');
 const audioCtx = new AudioContext();
 
-const littleEndian = (function() {
-  const buffer = new ArrayBuffer(2);
-  new DataView(buffer).setInt16(0, 256, true);
-  return new Int16Array(buffer)[0] === 256;
-})();
+let deferred;
 
-let timeoutId;
-const debounce = (fn, delay) => {
-  if (timeoutId) {
-    clearTimeout(timeoutId);
-  }
-  timeoutId = setTimeout(fn, delay);
-};
-
-const logSound = (name, gain, key) => {
-  console.clear();
-  console.dir();
-  console.dir(`${folder} ${name}`);
-  console.dir(`${speaker} ${gain}`);
-  console.dir(`${keyboard} [${key}]`.toUpperCase());
-  debounce(console.clear, 1000);
-};
-
-const playAudio = (soundBuffer, gain) => {
-  const src = audioCtx.createBufferSource();
-  src.buffer = soundBuffer;
-  const gainNode = audioCtx.createGain();
-  gainNode.gain.value = gain;
-  src.connect(gainNode);
-  gainNode.connect(audioCtx.destination);
-  src.start(0);
-};
-
-const attachTriggers = (name, {keys, gain}, soundBuffer) => {
-  window.addEventListener('keydown', (e) => {
-    const key = keys.find(k => {
-      return k.toUpperCase().codePointAt(0) === e.keyCode;
-    });
-    if (key) {
-      playAudio(soundBuffer, gain);
-      logSound(name, gain, key);
-    }
-  });
-};
-
-const loadSound = (url, done) => {
-  const xhr = new XMLHttpRequest();
-  xhr.open('GET', url, true);
-  xhr.responseType = 'arraybuffer';
-  xhr.onload = () => {
-
-    // the composition pattern is often used in buffers
-    const arr = new Uint16Array(xhr.response);
-    const view = new DataView(xhr.response);
-
-    // change sample rate
-//     view.setInt32(24, view.getInt32(24, littleEndian) * 8, littleEndian);
-//     arr[12] = arr[12] / 8;
-
-    audioCtx.decodeAudioData(xhr.response, done)
-  };
-  xhr.send();
-};
-
-const loadSoundMap = (path, soundMap) => {
-  for (const [name, config] of soundMap) {
-    const file = `sounds/${path}/${name}.wav`;
-    loadSound(file, attachTriggers.bind(ø, name, config));
-  }
-};
-
-
-
-const soundMap808 = new Map(Object.entries({
+const soundMap808 = asMap({
   'BD5025': {keys: ['a'], gain: 1},
   'SD1010': {keys: ['s'], gain: .3},
   'RS': {keys: ['d'], gain: .5},
@@ -90,22 +16,128 @@ const soundMap808 = new Map(Object.entries({
   'HC10': {keys: ['l'], gain: .3},
   'LC50': {keys: ['q'], gain: .6},
   'MA': {keys: ['w'], gain: .3},
-  'SD1075': {keys: ['e'], gain: .3},
+  'SD1075': {keys: ['e'], gain: .2},
   'MT75': {keys: ['r'], gain: .7},
   'CB': {keys: ['y'], gain: .3},
   'CL': {keys: ['u'], gain: .3},
   'HT50': {keys: ['i'], gain: .3},
   'LT50': {keys: ['o'], gain: .7},
   'MC50': {keys: ['p'], gain: .4},
-}));
+});
 
-const soundMapRock = new Map(Object.entries({
+const soundMapRock = asMap({
   'kick': {keys: ['d'], gain: 1},
   'snare': {keys: ['f'], gain: .7},
   'hihat': {keys: ['j', 'k'], gain: .3},
-}));
+});
+
+const soundMapSongs = asMap({
+  // copy a song file into public/sounds, and change the song name
+  'SONG_NAME': {keys: [' '], gain: .7}
+});
 
 
+function bootstrap() {
+  // loadSoundMap(soundMapRock, 'sounds/rock');
+  loadSoundMap(soundMap808, 'sounds/808');
+  // loadSoundMap(soundMapSongs);
+}
 
-// loadSoundMap('rock', soundMapRock);
-loadSoundMap('808', soundMap808);
+function loadSoundMap(soundMap, path = 'sounds') {
+  for (const entry of soundMap) {
+    let [name] = entry;
+    loadBuffer(`${path}/${name}.wav`,
+      manipulateWav.bind(ø,
+        decode.bind(ø,
+          bindKeys.bind(ø, entry, [playAudio, logSound]))));
+  }
+}
+
+function loadBuffer(url, done) {
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', url, true);
+  xhr.responseType = 'arraybuffer';
+  xhr.onload = () => done(xhr.response);
+  xhr.send();
+}
+
+// for reference: http://soundfile.sapp.org/doc/WaveFormat/
+function manipulateWav(next, buffer) {
+  const LE = isLittleEndian();
+
+  // the composition pattern is often used with buffers,
+  // it allows several views of the same buffer to be instantiated
+//   const view = new DataView(buffer);
+
+//   const sampleRate = view.getInt32(24, LE);
+//   view.setInt32(24, sampleRate >> 1, LE);
+
+  // another way of doing the same thing
+  // const arr = new Uint16Array(buffer);
+  // arr[12] = arr[12] / 8;
+
+  next(buffer);
+}
+
+function decode(next, buffer) {
+  audioCtx.decodeAudioData(buffer, next);
+}
+
+function bindKeys(entry, listeners, soundBuffer) {
+  const [, {keys}] = entry;
+  window.addEventListener('keydown', ({keyCode}) => {
+    const key = keys.find(hasKeyCode.bind(ø, keyCode));
+    if (key) {
+      listeners.forEach((l) => l(entry, soundBuffer, key));
+    }
+  });
+}
+
+function playAudio([, {gain}], soundBuffer) {
+  const src = audioCtx.createBufferSource();
+  src.buffer = soundBuffer;
+  const gainNode = audioCtx.createGain();
+  gainNode.gain.value = gain;
+  src.connect(gainNode);
+  gainNode.connect(audioCtx.destination);
+  src.start();
+}
+
+function logSound([name, {gain}], soundBuffer, key) {
+  const speaker = '\u{1f509}', folder = '\u{1f4c1}', keyboard = '\u{2328}';
+  console.clear();
+  console.dir(`
+ ${folder} '${name}'
+ ${speaker} ${gain}
+ ${keyboard} [${key.toUpperCase()}]`);
+  debounce(console.clear, 1500);
+}
+
+
+/* general utils */
+
+function asMap(obj) {
+  return new Map(Object.entries(obj));
+}
+
+function hasKeyCode(keyCode, key) {
+  return key.toUpperCase().codePointAt(0) === keyCode;
+}
+
+function debounce(fn, delay) {
+  if (deferred) {
+    clearTimeout(deferred);
+  }
+  deferred = setTimeout(fn, delay);
+}
+
+function isLittleEndian() {
+  const buffer = new ArrayBuffer(2);
+  new DataView(buffer).setInt16(0, 256, true);
+  return new Int16Array(buffer)[0] === 256;
+}
+
+
+/* go */
+
+bootstrap();
